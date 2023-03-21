38b9490494fc3e5035307fa6483901216eef2ff5
Ayshasambo <97912733+Ayshasambo@users.noreply.github.com>
2023-03-21 14:54:02 +0100

    Added Token.js

---
+++ models/Token.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const TokenSchema = new mongoose.Schema(
+    {
+            token: {
+                type: String
+            },
+    }
+
+);
+
+module.exports = mongoose.model("Token", TokenSchema);
