12da12f8ac2648674aa6ff54320d063ad7c8667c
Ayshasambo <aishamasambo94@gmail.com>
2023-04-03 14:10:58 +0100

    added bucket name

--- routes/impactbased.js
+++ routes/impactbased.js
@@ -145,7 +145,7 @@ router.get('*', async (req,res) => {
 
   try {
     let s3File = await s3.getObject({
-      Bucket: process.env.BUCKET,
+      Bucket: process.env.CYCLIC_BUCKET_NAME,
       Key: filename,
     }).promise()
 
