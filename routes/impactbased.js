3896d41e7b0c89e5de6f2eb14b8a229cf5b46982
Ayshasambo <aishamasambo94@gmail.com>
2023-04-03 14:42:19 +0100

    console.log for filename

--- routes/impactbased.js
+++ routes/impactbased.js
@@ -166,7 +166,7 @@ router.get('/', async (req,res) => {
 // curl -i -XPUT --data '{"k1":"value 1", "k2": "value 2"}' -H 'Content-type: application/json' https://some-app.cyclic.app/myFile.txt
 router.put('/', async (req,res) => {
   let filename = req.path.slice(1)
-
+  console.log(filename);
   console.log(typeof req.body)
 
   await s3.putObject({
