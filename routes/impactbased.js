d655005addad4b221ce98d2a8d73a7a22179a2f2
Ayshasambo <aishamasambo94@gmail.com>
2023-04-04 11:52:40 +0100

    revert key to original

--- routes/impactbased.js
+++ routes/impactbased.js
@@ -173,7 +173,7 @@ router.put('/', async (req,res) => {
   await s3.putObject({
     Body: JSON.stringify(req.body),
     Bucket: process.env.CYCLIC_BUCKET_NAME,
-    Key: tmp/W2.jpg//filename,
+    Key:filename,
   }).promise()
 
   res.set('Content-type', 'text/plain')
